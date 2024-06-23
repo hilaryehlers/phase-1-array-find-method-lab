7bf9fb1c20f18ac16f6c29a54a557784cc642293
Hilary Ehlers <hilarydixson@gmail.com>
2024-06-23 13:40:18 -0500

    arrayfind lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function superbowlWin(array) {
+    const winObject = array.find(obj => obj.result === "W");
+    if (winObject) {
+      return winObject.year;
+    }
+    return undefined;
+  }
